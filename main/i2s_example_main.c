c2d9523ca314604ee305769ac781e9cf905954bb
Nick Donaldson <ndonald2@gmail.com>
2022-03-29 17:14:13 -0600

    Realtime sine wave output

--- main/i2s_example_main.c
+++ main/i2s_example_main.c
@@ -18,73 +18,68 @@
 #include "esp_log.h"
 #include <math.h>
 
-
-#define SAMPLE_RATE     (36000)
+#define SAMPLE_RATE     (44100)
+#define DMA_BUF_LEN     (256)
+#define DMA_NUM_BUF     (2)
 #define I2S_NUM         (0)
-#define WAVE_FREQ_HZ    (100)
+#define WAVE_FREQ_HZ    (440)
 #define PI              (3.14159265)
-#define I2S_BCK_IO      (GPIO_NUM_4)
-#define I2S_WS_IO       (GPIO_NUM_5)
-#define I2S_DO_IO       (GPIO_NUM_18)
-#define I2S_DI_IO       (-1)
+#define PHASE_INC       (2.0 * PI * WAVE_FREQ_HZ / SAMPLE_RATE)
 
-#define SAMPLE_PER_CYCLE (SAMPLE_RATE/WAVE_FREQ_HZ)
+// #define SAMPLE_PER_CYCLE (SAMPLE_RATE/WAVE_FREQ_HZ)
 
 static const char* TAG = "i2s_example";
 
-static void setup_waves()
+// static void setup_waves()
+// {
+//     size_t buf_size = sizeof(uint16_t) * SAMPLE_PER_CYCLE * 2;
+//     uint16_t *samples_data = malloc(buf_size);
+//     unsigned int i, sample_val;
+//     double sin_float;
+//     size_t i2s_bytes_write = 0;
+
+//     printf("\r\nTest bits=%d free mem=%d, written data=%d\n", 16, esp_get_free_heap_size(), buf_size);
+
+//     // triangle_float = -(pow(2, bits)/2 - 1);
+
+//     for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
+//         sin_float = (sin(i * 2 * PI / SAMPLE_PER_CYCLE) + 1.0) * 0.5;
+//         sin_float *= 255.0;
+//         samples_data[i*2] = samples_data[i*2+1] = (uint16_t)sin_float << 8;
+//     }
+
+//     ESP_LOGI(TAG, "write data");
+//     i2s_write(I2S_NUM, samples_data, buf_size, &i2s_bytes_write, 100);
+//     ESP_LOGI(TAG, "written bytes: %d", i2s_bytes_write);
+
+//     free(samples_data);
+// }
+
+static float p = 0.0;
+static int count = 0;
+static uint16_t out_buf[DMA_BUF_LEN * 2];
+
+static void write_buffer()
 {
-    size_t buf_size = sizeof(uint16_t) * SAMPLE_PER_CYCLE * 2;
-    uint16_t *samples_data = malloc(buf_size);
-    unsigned int i, sample_val;
-    double sin_float;
-    // double sin_float, triangle_float, triangle_step = (double) pow(2, bits) / SAMPLE_PER_CYCLE;
-    size_t i2s_bytes_write = 0;
+    float samp = 0.0;
+    size_t bytes_written;
 
-    printf("\r\nTest bits=%d free mem=%d, written data=%d\n", 16, esp_get_free_heap_size(), buf_size);
-
-    // triangle_float = -(pow(2, bits)/2 - 1);
-
-    for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
-        sin_float = (sin(i * 2 * PI / SAMPLE_PER_CYCLE) + 1.0) * 0.5;
-        // if(sin_float >= 0)
-        //     triangle_float += triangle_step;
-        // else
-        //     triangle_float -= triangle_step;
-
-        sin_float *= 255.0;
-        samples_data[i*2] = samples_data[i*2+1] = (uint16_t)sin_float << 8;
-
-        // if (bits == 16) {
-        //     sample_val = 0;
-        //     sample_val += (short)triangle_float;
-        //     sample_val = sample_val << 16;
-        //     sample_val += (short) sin_float;
-        //     samples_data[i] = sample_val;
-        // } else if (bits == 24) { //1-bytes unused
-        //     samples_data[i*2] = ((int) triangle_float) << 8;
-        //     samples_data[i*2 + 1] = ((int) sin_float) << 8;
-        // } else {
-        //     samples_data[i*2] = ((int) triangle_float);
-        //     samples_data[i*2 + 1] = ((int) sin_float);
-        // }
-
+    for (int i=0; i < DMA_BUF_LEN; i++) {
+        samp = (sinf(p) + 1.0) * 0.5;
+        p += PHASE_INC;
+        if (p >= 2.0*PI)
+            p -= 2.0*PI;
+        
+        samp *= 255.0;
+        out_buf[i*2] = out_buf[i*2+1] = (uint16_t)samp << 8;
     }
-    // ESP_LOGI(TAG, "set clock");
-    // i2s_set_clk(I2S_NUM, SAMPLE_RATE, bits, 2);
-    //Using push
-    // for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
-    //     if (bits == 16)
-    //         i2s_push_sample(0, &samples_data[i], 100);
-    //     else
-    //         i2s_push_sample(0, &samples_data[i*2], 100);
-    // }
-    // or write
-    ESP_LOGI(TAG, "write data");
-    i2s_write(I2S_NUM, samples_data, buf_size, &i2s_bytes_write, 100);
-    ESP_LOGI(TAG, "written bytes: %d", i2s_bytes_write);
 
-    free(samples_data);
+    i2s_write(I2S_NUM, out_buf, sizeof(out_buf), &bytes_written, portMAX_DELAY);
+}
+
+static void audio_task(void *user)
+{
+    while(1) write_buffer();
 }
 
 void app_main(void)
@@ -100,15 +95,15 @@ void app_main(void)
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
         .communication_format = I2S_COMM_FORMAT_STAND_MSB,
-        .dma_buf_count = 6,
-        .dma_buf_len = 60,
+        .dma_buf_count = DMA_NUM_BUF,
+        .dma_buf_len = DMA_BUF_LEN,
         .use_apll = false,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1                                //Interrupt level 1
     };
     i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
     i2s_set_pin(I2S_NUM, NULL);
 
-    setup_waves();
+    xTaskCreate(audio_task, "audio", 4096, NULL, configMAX_PRIORITIES - 1, NULL);
 
     while (1) {
         vTaskDelay(5000/portTICK_PERIOD_MS);
